19118a398f5ee8724e836ab0b570b47fedb672d5
march-dave <ysangy@gmail.com>
2016-05-08 14:20:40 -0700

    index switch to ejs

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,14 @@ var router = express.Router();
 var path = require('path');
 
 
+// router.get('/', function(req, res, next) {
+//   var indexPath = path.join(__dirname, '../views/index.html');
+//   res.sendFile(indexPath);
+// });
+
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  var indexPath = path.join(__dirname, '../views/index.html');
-  res.sendFile(indexPath);
+  res.render('index');
+
 });
 
 module.exports = router;
